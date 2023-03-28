aaca179a139caa5b0a173696d02d5711ec81d60f
gokcandemiralp <gok.can.13@gmail.com>
2023-03-29 01:38:36 +0300

    first succesful render

--- vert.glsl
+++ vert.glsl
@@ -36,15 +36,11 @@ void main(void){
 
 	vec3 lightPos, I, L, H;
 	float NdotL, NdotH;
-	float distanceModifier = 0.00001;
 	vec3 diffuseColor  = vec3(0,0,0);
 	vec3 specularColor = vec3(0,0,0);
 	for(int i = 0 ; i<lightCount ; ++i){
 		lightPos = pointLights[i].position;	// light position in world coordinates
-		distanceModifier += (vP.x - lightPos.x)*(vP.x - lightPos.x) 
-						  + (vP.y - lightPos.y)*(vP.y - lightPos.y) 
-						  + (vP.z - lightPos.z)*(vP.z - lightPos.z);
-		I = pointLights[i].color / distanceModifier ;   		// point light intensity
+		I = pointLights[i].color;   		// point light intensity
 		L = normalize(lightPos - vec3(pWorld));
 		H = normalize(L + V);
 		NdotL = dot(N, L); // for diffuse component
